2779cc6789a9730ca309e16bb69d66990c7d98cf
Julius <julius.r.hopf@gmail.com>
2020-08-06 18:41:21 +0200

    Removed excessive code

--- myScript.js
+++ myScript.js
@@ -1,17 +1,22 @@
 //const fetch = require('node-fetch');
 var tag = "";
 
-//gets tag from redirected URL returns token as string
+/**
+ * Gets the access token from the url
+ * @returns {string} with access token in it
+ */
+
 async function getTag(){
+
   async function getHashParams() {
-          var hashParams = {};
-          var e, r = /([^&;=]+)=?([^&;]*)/g,
-              q = window.location.hash.substring(1);
-          while ( e = r.exec(q)) {
-             hashParams[e[1]] = decodeURIComponent(e[2]);
-          }
-          return hashParams;
-        };
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  };
 
   var params = await getHashParams();
 
@@ -19,9 +24,13 @@ async function getTag(){
   return access_token;
 }
 
-//fetches data at input endpoint returns as parsed json
-async function fetchData(input){
-    const url = input;
+
+/**
+ * @param {string} endpoint fetches data at endpoint
+ * @returns {string} result as parsed json
+ */
+async function fetchData(endpoint){
+    const url = endpoint;
     const options = {
       headers: {
         Authorization: "Bearer ".concat(tag)
@@ -33,43 +42,39 @@ async function fetchData(input){
       return result;
 }
 
-//extracts all the playlist ids from the
-async function getPlaylistIdFromUsersPlaylists(item) {
+//
+/**
+ * Extracts all the playlist ids
+ * @param {array} lists a list of playlists
+ * @returns {array} temp a list of list ids
+ */
+async function getPlaylistIdFromUsersPlaylists(lists) {
   var temp = [];
-  item.forEach(async function(it){
-    await temp.push(it['id']);
+  lists.forEach(async function(list){
+    await temp.push(list['id']);
   });
   return temp;
 }
 
+/**
+ * Gets all tracks from all playlists
+ * @param {array} playlist a list of playlist ids
+ * @returns {array} temp all tracks in playlists
+ */
 async function getAllTracks(playlists) {
   var temp = [];
   await Promise.all(playlists.map(async (playlist) => {
-      var result = await getPlaylistTracks("https://api.spotify.com/v1/playlists/"+playlist+"/tracks");
+      var result = await fetchData("https://api.spotify.com/v1/playlists/"+playlist+"/tracks");
       temp.push(...Object.values(result)[1]);
 
       while(result['next'] !== null){
-        let temp2 = await getPlaylistTracks(result['next']);
+        let temp2 = await fetchData(result['next']);
         temp.push(...Object.values(temp2)[1]);
         result = temp2;
       }
   }));
   return temp;
 }
-
-async function getPlaylistTracks(link) {
-  const url = link;
-  const options = {
-    headers: {
-      Authorization: "Bearer ".concat(tag)
-    }
-  };
-
-  const result = fetch(url,options)
-    .then(result => result.json())
-    return result;
-}
-
 
 async function getAllArtists(tracklist){
   var map = new Map();
@@ -128,7 +133,7 @@ async function getNamesAndImages(ids) {
   var temp = [];
   await Promise.all(ids.map(async (id) => {
 
-      var result = await getArtist("https://api.spotify.com/v1/artists/"+id['id']);
+      var result = await fetchData("https://api.spotify.com/v1/artists/"+id['id']);
       if(result.hasOwnProperty('images')){
         temp.push({'id': id['id'], 'name': result['name'], 'value': id['value'], 'image': result['images'][0]});
         console.log(result);
@@ -148,21 +153,6 @@ async function getNamesAndImages(ids) {
   return temp;
 }
 
-async function getArtist(link) {
-  const url = link;
-  const options = {
-    headers: {
-      Authorization: "Bearer ".concat(tag)
-    }
-  };
-
-  const result = fetch(url,options)
-    .then(result => result.json())
-    return result;
-}
-
-
-
 const main = async () => {
   //get tag from URL
   tag = await getTag();
@@ -171,7 +161,7 @@ const main = async () => {
   var listsOfPlaylists = [];
   let temp = await fetchData("https://api.spotify.com/v1/me/playlists");
   listsOfPlaylists.push(...Object.values(temp)[1]);
-  //keeps getting lists while theres is more lists
+  //keeps getting lists while there is more lists
   while(temp['next'] !== null){
     let temp2 = await fetchData(temp['next']);
     listsOfPlaylists.push(...Object.values(temp2)[1]);
